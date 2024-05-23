f961aec96b4c669d300c1fe94a5a4fdb58982ced
richmondazadze <richmondazadze1313@gmail.com>
2024-05-23 23:01:11 +0000

    Contact Form Changes

---
+++ index.js
@@ -0,0 +1,21 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_rycugzh";
+  const templateID = "template_4bp2dil";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
